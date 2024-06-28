46e4f68de3625567e47546f902fdf6e4dfd25e5f
muhamad satria <114433514+mhmdsatria@users.noreply.github.com>
2024-06-28 13:23:58 +0700

    Update Santri.java

--- Santri.java
+++ Santri.java
@@ -11,5 +11,48 @@ package PembayaranKitab;
  * @author marse
  */
 public class Santri {
-    
+    private int idsantri;
+    private String nama;
+    private String kobong;
+
+    public Santri(int idsantri, String nama, String kobong) {
+        this.idsantri = idsantri;
+        this.nama = nama;
+        this.kobong = kobong;
+    }
+
+    // Getter and setter methods
+    public int getIdsantri() {
+        return idsantri;
+    }
+
+    public void setIdsantri(int idsantri) {
+        this.idsantri = idsantri;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKobong() {
+        return kobong;
+    }
+
+    public void setKobong(String kobong) {
+        this.kobong = kobong;
+    }
+
+    @Override
+    public String toString() {
+        return "Santri{" +
+                "idsantri=" + idsantri +
+                ", nama='" + nama + '\'' +
+                ", kobong='" + kobong + '\'' +
+                '}';
+    }
 }
+
